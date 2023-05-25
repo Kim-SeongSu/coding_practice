bdfe245c44bc5746e861e738504809e2a17f95b0
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-05-25 19:17:21 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -0,0 +1,16 @@
+'''
+import sys
+for line in sys.stdin:
+    print(line, end="")
+'''
+
+print(open(0).read())
+
+
+'''
+while True:
+    try:
+        print(input())
+    except:
+        break
+'''
