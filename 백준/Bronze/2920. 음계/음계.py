96349e96bc536b6c9c650281cb87cb31ee0cf0ab
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-05-25 20:08:52 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,6 +1,6 @@
-if (I:=list(map(int,input().split()))) == [1, 2, 3, 4, 5, 6, 7, 8]:
+if (I:=input()) == '1 2 3 4 5 6 7 8':
     print('ascending')
-elif I == [8, 7, 6, 5, 4, 3, 2, 1]:
+elif I == '8 7 6 5 4 3 2 1':
     print('descending')
 else:
     print('mixed')
