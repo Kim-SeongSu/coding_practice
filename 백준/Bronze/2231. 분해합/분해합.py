2c65bb3296bee8c57a9fc78b2072a80cfe115776
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-05-26 21:20:34 +0900

    Update 분해합.py
    풀이 의문점 추가

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,5 +1,14 @@
 N = int(input())
 
+'''
+시간을 단축시키기 위해 아래와 같이 반복문 range 시작값을 정해줬는데 오류가 발생함.
+
+이유는 아직 모름
+G = N - int('999999'[:len(str(N))-1]) if N > 10 else N
+
+for i in range(G,N+1):
+'''
+
 for i in range(1,N+1):
     S = sum(list(map(int,str(i))))
 
