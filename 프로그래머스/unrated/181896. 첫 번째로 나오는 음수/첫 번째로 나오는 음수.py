2385803e06ba1e4e7c83909dd4b45e0dad469df1
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-06-02 13:17:48 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,8 +1,5 @@
 def solution(num_list):
-    while True:
         for i in num_list:
             if i < 0:
                 return num_list.index(i)
-                break
         return -1
-        break
