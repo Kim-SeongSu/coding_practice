001c97eeb2f12d3903f3b3e93a26c551ee9eec34
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-06-02 12:35:07 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,5 +1,2 @@
 def solution(num_list):
-    temp = 1
-    for i in num_list:
-        temp *= i
-    return int(temp < sum(num_list)**2)
+    return int(eval('*'.join([str(i) for i in num_list])) < sum(num_list)**2)
