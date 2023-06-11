4cf828ff5c0fb9368ed0fb87b786a66216ae7c0f
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-06-11 23:28:22 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    x,result = n,[]
+    result.append(x)
+    while x != 1:
+        if x % 2 ==0:
+            x /= 2
+        else:
+            x = 3*x+1
+        result.append(x)
+        
+    return result
