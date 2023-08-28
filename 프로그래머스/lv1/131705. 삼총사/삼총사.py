bec49466321f39c9542d09461c97ce0b6c83ffc9
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-08-28 23:11:23 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,9 +1,6 @@
 def solution(number):
-    result = 0
-    
-    for i in range(n:=len(number)):
-        for j in range(n):
-            for k in range(n):
-                if (i!=j!=k) and (i!=k) and (number[i]+number[j]+number[k] == 0):
-                    result +=1
-    return result//6
+    import itertools
+
+    arr = [sum(i) for i in itertools.combinations(number,3) if sum(i) == 0]
+          
+    return len(arr)
