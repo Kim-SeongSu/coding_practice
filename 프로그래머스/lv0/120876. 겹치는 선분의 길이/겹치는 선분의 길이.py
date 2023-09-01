a8cf145eca244ff66b84caba7de4b57807b70aac
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-09-02 07:36:07 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,3 +1,4 @@
+'''
 def abc(a,b):
     arr = sorted([i for i in set(a+b) if (a+b).count(i) == 2])
     return arr if len(arr) > 1 else []
@@ -8,3 +9,8 @@ def solution(lines):
     arr = sorted(set(abc(a,b)+abc(b,c)+abc(c,a)))
 
     return sum([1 for i in range(len(arr)-1) if arr[i+1]-arr[i] == 1])
+'''
+
+def solution(lines):
+    a,b,c = [set(range(i[0],i[1])) for i in lines]
+    return len(a&b|b&c|c&a)
