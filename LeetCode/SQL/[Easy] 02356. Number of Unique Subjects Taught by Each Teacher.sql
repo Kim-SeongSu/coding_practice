c30613a17c92d367fab9baf5d710d284abf149bd
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2024-02-28 13:58:28 +0900

    Upload: LeetCode problem solves
    leetcode 문제 풀이 업로드
    - 2356
    - 1141
    - 1174

---
+++ LeetCode/SQL/[Easy] 02356. Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,4 @@
+# MySQL & postgreSQL
+select teacher_id , count(distinct subject_id ) cnt
+from Teacher
+group by teacher_id
---
+++ LeetCode/SQL/[Medium] 01174. Immediate Food Delivery II.sql
@@ -0,0 +1,13 @@
+/*
+# MySQL
+select round(100*avg(order_date = customer_pref_delivery_date ),2) immediate_percentage 
+from Delivery
+where (customer_id, order_date) in (select customer_id, min(order_date)
+                                    from Delivery 
+                                    group by customer_id)
+*/
+
+-- # postgreSQL
+select round(avg(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100, 2) immediate_percentage
+from Delivery
+where (customer_id, order_date) in (Select customer_id, min(order_date) from Delivery group by customer_id)
