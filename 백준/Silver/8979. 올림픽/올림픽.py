7a163725c8eb09ce709f1c8b58e9ee56c9b467f4
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2024-03-16 14:09:25 +0900

    [Silver V] Title: 올림픽, Time: 132 ms, Memory: 110272 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,7 +1,11 @@
 N, K = map(int,input().split())
-score = {}
-for _ in range(N):
-    nation, *medal = input().split()
-    score[nation] = int(''.join(medal))
-    
-print(sorted(score, key= lambda x:score[x] ,reverse=True).index(str(K)))
+score = [list(map(int,input().split())) for _ in range(N)]
+score.sort(key= lambda x:(-x[1], -x[2], -x[3]))
+
+for i in range(N):
+    if score[i][0] == K:
+        index = i
+for i in range(N):
+    if score[index][1:] == score[i][1:]:
+        print(i + 1)
+        break
