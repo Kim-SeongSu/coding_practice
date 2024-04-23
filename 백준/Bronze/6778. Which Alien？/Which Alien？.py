2574a376cd229e0cc3ea8c9c12fbfcb58457a4bb
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2024-04-23 14:47:43 +0900

    [Bronze IV] Title: Which Alien?, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,7 @@
+antenna, eyes = int(input()),int(input())
+if antenna > 2 and eyes < 5:
+  print('TroyMartian')
+if antenna < 7 and eyes > 1:
+  print('VladSaturnian')
+if antenna < 3 and eyes < 4:
+  print('GraemeMercurian')
