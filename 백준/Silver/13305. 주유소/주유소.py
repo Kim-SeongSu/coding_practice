43007903f073c3fefc9929c7ca52ea1d6aa21091
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2024-05-26 21:50:43 +0900

    [Silver III] Title: 주유소, Time: 160 ms, Memory: 137180 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,5 +1,17 @@
+'''
+# 41점 (시간 초과)
 N = int(input())
 dist = list(map(int,input().split()))[::-1]
 cost = list(map(int,input().split()))[::-1][1:]
-
 print(sum([min(cost[i:])*dist[i] for i in range(N-1)]))
+'''
+
+N = int(input())
+dist = list(map(int,input().split()))
+cost = list(map(int,input().split()))[:-1]
+min_c, t = 9999999999, 0
+for i in range(N-1):
+    if cost[i]<min_c:
+        min_c = cost[i]
+    t += dist[i]*min_c
+print(t)
