a5a2ce1efea950cdc447ed19c7b2c7eb2e5485ee
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2024-07-24 09:35:04 +0900

    [Bronze I] Title: A/B - 2, Time: 468 ms, Memory: 34304 KB, Score: 2000 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A=sc.nextInt(), B=sc.nextInt();
+
+        System.out.printf("%d.",A/B);
+
+        for (int i=0; i<2001; i++) {
+            A = (A%B)*10;
+            System.out.printf("%d",A/B);
+            
+            if (A%B==0){
+                break;
+            }
+        }
+        
+        sc.close();
+    }
+}
