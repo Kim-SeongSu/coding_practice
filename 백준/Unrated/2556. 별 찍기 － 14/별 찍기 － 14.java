7430713eb7a2d82e4f533fed0df9613b881b9668
kimss024 <104110605+Kim-SeongSu@users.noreply.github.com>
2024-09-17 14:56:57 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 232 ms, Memory: 15716 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                System.out.print('*');
+                    }
+            System.out.println();
+        }
+        br.close();
+    }
+}
